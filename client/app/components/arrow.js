bf63ed87e2fdaabf6113f01afa2bfa2673d89844
Isaac <isaacxpreston@gmail.com>
2017-03-08 23:20:05 -0800

    animations on load

--- client/app/components/arrow.js
+++ client/app/components/arrow.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import Radium from 'radium'
 
+const fade_in = Radium.keyframes({
+  "0%": {
+    opacity: 0
+  },
+  "100%": {
+    opacity: 1
+  }
+})
 
 const bounce = Radium.keyframes({
   "0%": {
@@ -49,7 +57,13 @@ const Arrow = React.createClass({
       }}
       onClick={this.scroll}>
         <svg 
-          width="0.125in" height="0.653in">
+          width="0.125in" height="0.653in"
+          style={{
+            opacity: 0,
+            animation: "x 1s ease forwards",
+            animationName: fade_in,
+            animationDelay: "3.5s"
+          }}>
           <path fillRule="evenodd"  fill="rgb(255, 255, 255)"
           d="M4.496,46.992 L8.986,36.151 L6.054,37.363 C6.054,22.040 6.054,16.753 6.054,1.433 C6.054,-0.509 2.946,-0.509 2.946,1.433 C2.946,16.751 2.946,22.037 2.946,37.357 L0.014,36.128 L4.496,46.992 Z"/>
         </svg>
--- client/app/components/splash.js
+++ client/app/components/splash.js
@@ -5,6 +5,28 @@ import Component from './component'
 import Arrow from './arrow'
 import Button from './button'
 
+const slide_in = Radium.keyframes({
+  "0%": {
+    opacity: 0,
+    transform: "translateX(-100%)"
+  },
+  "100%": {
+    opacity: 1,
+    transform: "none"
+  }
+})
+
+const slide_up = Radium.keyframes({
+  "0%": {
+    opacity: 0,
+    transform: "translateY(100%)"
+  },
+  "100%": {
+    opacity: 1,
+    transform: "none"
+  }
+})
+
 const styles = {
   
   splash: {
@@ -12,7 +34,6 @@ const styles = {
     marginBottom: "auto",
     marginLeft: "auto",
     '@media (max-width: 1000px)': {
-      // marginTop: "2.5em",
       marginLeft: "auto",
       marginRight: "auto"
     },
@@ -23,9 +44,12 @@ const styles = {
   
   header: {
     fontSize: "3em",
+    opacity: 0,
     textTransform: "uppercase",
+    animation: "x 0.8s ease forwards",
+    animationName: slide_in,
     color: "white",
-    '@media (max-width: 450px)': { //also 330 width for banner and header fonts
+    '@media (max-width: 450px)': {
       fontSize: "2em"
     },
     '@media (max-height: 380px)': {
@@ -33,8 +57,16 @@ const styles = {
     }
   },
   subheader: {
+    opacity: 0,
+    animation: "x 0.8s ease forwards",
+    animationName: slide_in,
     fontSize: "0.85em",
     color: "white"
+  },
+  button_wrapper: {
+    opacity: 0,
+    animation: "x 0.8s ease forwards",
+    animationName: slide_up,
   },
   boldface: {
     fontWeight: "bold",
@@ -54,31 +86,31 @@ const MainSplash = React.createClass({
     this.setState({
       scroll: (window.innerHeight - (window.scrollY * 1.5)) / window.innerHeight
     })
-    
     window.addEventListener('scroll', this.updateScroll)
   },
 
   render () {
     return (
+      //fix component
       <Component opacity={this.state.scroll} resize>
         <div style={styles.splash}>
-          <div style={styles.header}>
+          <div style={{...styles.header, animationDelay: "0.25s"}}>
             from <span style={styles.boldface}> factory </span>
           </div>
-          <div style={{marginTop: "-5px", ...styles.header}}>
+          <div style={{marginTop: "-5px", ...styles.header, animationDelay: "1s"}}>
             to <span style={styles.boldface}> store </span>
           </div>
-          <div style={{marginTop: "-5px", ...styles.header}}>
+          <div style={{marginTop: "-5px", ...styles.header, animationDelay: "1.75s"}}>
             to <span style={styles.boldface}> out the door. </span>
           </div>
-          <div style={{marginTop: "15px", marginBottom: "5px", ...styles.subheader}}>
+          <div style={{marginTop: "15px", marginBottom: "5px", ...styles.subheader, animationDelay: "2.5s"}}>
             ABC Creations offers
             <span style={styles.boldface}> international end-to-end apparel</span>
           </div>
-          <div style={{marginBottom: "20px", ...styles.subheader}}>
+          <div style={{marginBottom: "20px", ...styles.subheader, animationDelay: "2.5s"}}>
             manufacturing solutions for forward thinking clothing brands.
           </div>
-          <div>
+          <div style={{...styles.button_wrapper, animationDelay: "2.5s"}}>
             <Button>our process</Button>
             <Button>our products</Button>
           </div>
